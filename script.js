4f0c9e1f248a0993d4906b904bda906c4afdd086
Manideep757 <gollapellymanideep185@gmail.com>
2026-05-04 23:35:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+   let total = 0;
+   let prices = document.querySelectorAll(".price")
+	prices.forEach(function(price){
+		total += Number(price.textContent.trim());
+	})
+	let table = document.getElementById("grocery-table");
+	let tr = document.createElement("tr");
+	let td = document.createElement("td");
+	td.innerText = "Total:"+total;
+	td.colSpan = 2;
+	tr.appendChild(td);
+    table.appendChild(tr);
+	
 };
 
 getSumBtn.addEventListener("click", getSum);
